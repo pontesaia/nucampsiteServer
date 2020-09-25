5676a9b3b262613a63addc32ad8a86b1c514b2fb
Diane Saia <pontesaia@gmail.com>
2020-09-25 19:07:57 -0400

    Passport JWT

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const authenticate = require("../authenticate");
 
 const partnerRouter = express.Router();
 partnerRouter.use(bodyParser.json());
@@ -8,41 +9,41 @@ partnerRouter
   .get((req, res) => {
     res.end("Will send all the partners to you");
   })
-  .post((req, res) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     res.end(
       `Will add the partner: ${req.body.name} with description: ${req.body.description}`
     );
   })
-  .put((req, res) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end("PUT operation not supported on /partners");
   })
-  .delete((req, res) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     res.end("Deleting all partners");
   });
 
 partnerRouter
   .route("/:partnerId")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
-  })
+  // .all((req, res, next) => {
+  //   res.statusCode = 200;
+  //   res.setHeader("Content-Type", "text/plain");
+  //   next();
+  // })
   .get((req, res) => {
     res.end(`Will send details of the partner: ${req.params.partnerId} to you`);
   })
-  .post((req, res) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end(
       `POST operation not supported on /partners/${req.params.partnerId}`
     );
   })
-  .put((req, res) => {
+  .put(authenticate.verifyUser, (req, res, next) => {
     res.write(`Updating the partner: ${req.params.partnerId}\n`);
     res.end(`Will update the partner: ${req.body.name}
           with description: ${req.body.description}`);
   })
-  .delete((req, res) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     res.end(`Deleting partner: ${req.params.partnerId}`);
   });
 
